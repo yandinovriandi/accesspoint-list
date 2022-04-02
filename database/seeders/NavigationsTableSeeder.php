f42e3c591fde6cadf1ee5be7763077779b166ff7
yandinovriandi <yandinovriandi@gmail.com>
2022-04-02 23:52:14 +0700

    UpdateDatabaseSeeder.php

--- database/seeders/NavigationsTableSeeder.php
+++ database/seeders/NavigationsTableSeeder.php
@@ -16,52 +16,54 @@ class NavigationsTableSeeder extends Seeder
     public function run()
     {
         collect([
-            'name' => 'Navigations Sidebar',
-            'permission_name' => 'create navigations',
-        ],
-        [
-            'name' => 'Role & Permissions',
-            'permission_name' => 'create permissions',
-        ],
-        [
-            'name' => 'Devices',
-            'permission_name' => 'create devices',
-        ],
-        [
-            'parent_id' => 1,
-            'name' => 'Table',
-            'permission_name' => 'create navigations',
-            'url' => 'settings/navigations-sidebar'
-        ],
-        [
-            'parent_id' => 2,
-            'name' => 'Roles',
-            'permission_name' => 'create permissions',
-            'url' => 'settings/role-and-permission/roles'
-        ],
-        [
-            'parent_id' => 2,
-            'name' => 'Permissions',
-            'permission_name' => 'create permissions',
-            'url' => 'settings/role-and-permission/permissions'
-        ],
-        [
-            'parent_id' => 2,
-            'name' => 'Assign Permissions',
-            'permission_name' => 'create permissions',
-            'url' => 'settings/role-and-permission/assign'
-        ],
-        [
-            'parent_id' => 2,
-            'name' => 'Assign Permissions to User',
-            'permission_name' => 'create permissions',
-            'url' => 'settings/role-and-permission/assign/user'
-        ],
-        [
-            'parent_id' => 3,
-            'name' => 'Table',
-            'permission_name' => 'create devices',
-            'url' => 'settings/devices'
+            [
+                'name' => 'Navigations Sidebar',
+                'permission_name' => 'create navigations',
+            ],
+            [
+                'name' => 'Role & Permissions',
+                'permission_name' => 'create permissions',
+            ],
+            [
+                'name' => 'Devices',
+                'permission_name' => 'create devices',
+            ],
+            [
+                'parent_id' => 1,
+                'name' => 'Table',
+                'permission_name' => 'create navigations',
+                'url' => 'settings/navigations-sidebar'
+            ],
+            [
+                'parent_id' => 2,
+                'name' => 'Roles',
+                'permission_name' => 'create permissions',
+                'url' => 'settings/role-and-permission/roles'
+            ],
+            [
+                'parent_id' => 2,
+                'name' => 'Permissions',
+                'permission_name' => 'create permissions',
+                'url' => 'settings/role-and-permission/permissions'
+            ],
+            [
+                'parent_id' => 2,
+                'name' => 'Assign Permissions',
+                'permission_name' => 'create permissions',
+                'url' => 'settings/role-and-permission/assign'
+            ],
+            [
+                'parent_id' => 2,
+                'name' => 'Assign Permissions to User',
+                'permission_name' => 'create permissions',
+                'url' => 'settings/role-and-permission/assign/user'
+            ],
+            [
+                'parent_id' => 3,
+                'name' => 'Table',
+                'permission_name' => 'create devices',
+                'url' => 'settings/devices'
+            ]
         ])->each(fn ($q) => Navigation::create($q));
     }
 }
