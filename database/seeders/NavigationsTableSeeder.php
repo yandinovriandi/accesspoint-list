d0c42c8a00591eee3949071d17b6784be8efcf81
Irsyad A. Panjaitan <irsyadpanjaitan@gmail.com>
2022-04-02 22:27:10 +0700

    Update NavigationsTableSeeder.php

--- database/seeders/NavigationsTableSeeder.php
+++ database/seeders/NavigationsTableSeeder.php
@@ -15,62 +15,53 @@ class NavigationsTableSeeder extends Seeder
      */
     public function run()
     {
-        $nav1 = new Navigation;
-        $nav1->create([
+        collect([
             'name' => 'Navigations Sidebar',
             'permission_name' => 'create navigations',
-        ]);
-        $nav2 = new Navigation;
-        $nav2->create([
+        ],
+        [
             'name' => 'Role & Permissions',
             'permission_name' => 'create permissions',
-        ]);
-        $nav3 = new Navigation;
-        $nav3->create([
+        ],
+        [
             'name' => 'Devices',
             'permission_name' => 'create devices',
-        ]);
-        $navigation = new Navigation;
-        $navigation->create([
+        ],
+        [
             'parent_id' => 1,
             'name' => 'Table',
             'permission_name' => 'create navigations',
             'url' => 'settings/navigations-sidebar'
-        ]);
-        $navigation2 = new Navigation;
-        $navigation2->create([
+        ],
+        [
             'parent_id' => 2,
             'name' => 'Roles',
             'permission_name' => 'create permissions',
             'url' => 'settings/role-and-permission/roles'
-        ]);
-        $navigation3 = new Navigation;
-        $navigation3->create([
+        ],
+        [
             'parent_id' => 2,
             'name' => 'Permissions',
             'permission_name' => 'create permissions',
             'url' => 'settings/role-and-permission/permissions'
-        ]);
-        $navigation4 = new Navigation;
-        $navigation4->create([
+        ],
+        [
             'parent_id' => 2,
             'name' => 'Assign Permissions',
             'permission_name' => 'create permissions',
             'url' => 'settings/role-and-permission/assign'
-        ]);
-        $navigation5 = new Navigation;
-        $navigation5->create([
+        ],
+        [
             'parent_id' => 2,
             'name' => 'Assign Permissions to User',
             'permission_name' => 'create permissions',
             'url' => 'settings/role-and-permission/assign/user'
-        ]);
-        $navigation6 = new Navigation;
-        $navigation6->create([
+        ],
+        [
             'parent_id' => 3,
             'name' => 'Table',
             'permission_name' => 'create devices',
             'url' => 'settings/devices'
-        ]);
+        ])->each(fn ($q) => Navigation::create($q));
     }
 }
